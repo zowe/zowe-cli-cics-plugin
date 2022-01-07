38a281f5656bd6a12d44c9d06f1622c920af34bc
zFernand0 <fernando.rijocedeno@broadcom.com>
2022-01-07 12:31:28 +0000

    Introduce test commands to the zowe auth login/logout groups
    Signed-off-by: zFernand0 <fernando.rijocedeno@broadcom.com>

---
+++ src/cli/get/resource/Test.login.handler.ts
@@ -0,0 +1,27 @@
+/*
+* This program and the accompanying materials are made available under the terms of the *
+* Eclipse Public License v2.0 which accompanies this distribution, and is available at *
+* https://www.eclipse.org/legal/epl-v20.html                                      *
+*                                                                                 *
+* SPDX-License-Identifier: EPL-2.0                                                *
+*                                                                                 *
+* Copyright Contributors to the Zowe Project.                                     *
+*                                                                                 *
+*/
+
+import { AbstractSession, IHandlerParameters, IProfile, RestClient, TextUtils } from "@zowe/imperative";
+import { CicsBaseHandler } from "../../CicsBaseHandler";
+
+/**
+ * Handler to show zosmf information
+ * @export
+ * @class Handler
+ * @implements {ICommandHandler}
+ */
+export default class Handler extends CicsBaseHandler {
+    public async processWithSession(params: IHandlerParameters, session: AbstractSession, profile: IProfile): Promise<any> {
+        params.response.console.log("\n\nTest message: LOGIN to CICS\n");
+        const zosResponse: any = await RestClient.getExpectJSON(session, "/zosmf/info", [{ "X-CSRF-ZOSMF-HEADER": true }]);
+        params.response.console.log(TextUtils.prettyJson(zosResponse));
+    }
+}
--- src/imperative.ts
+++ src/imperative.ts
@@ -14,6 +14,26 @@ import { IImperativeConfig } from "@zowe/imperative";
 import { PluginConstants } from "./api/constants/PluginConstants";
 import { CicsSession } from "./cli/CicsSession";
 
+const tempLogin = {
+    description: "Test login to CICS",
+    summary: "Login to CICS",
+    examples: [
+        {
+            description: "Login to CICS",
+            options: ""
+        }
+    ]
+};
+const tempLogout = {
+    description: "Test logout from CICS",
+    summary: "Logout from CICS",
+    examples: [
+        {
+            description: "Logout from CICS",
+            options: ""
+        }
+    ],
+};
 const config: IImperativeConfig = {
     commandModuleGlobs: ["**/cli/*/*.definition!(.d).*s"],
     rootCommandDescription: PluginConstants.PLUGIN_DESCRIPTION,
@@ -27,8 +47,32 @@ const config: IImperativeConfig = {
             connProfType: "cics"
         }
     ],
+    authGroupConfig: {
+        authGroup: {
+            description: "Test authentication to CICS service through zowe auth login",
+            summary: "Authenticate to CICS"
+        },
+        loginGroup: tempLogin,
+        logoutGroup: tempLogout
+    },
     profiles: [
         {
+            authConfig: [
+                {
+                    serviceName: "cics-001",
+                    // aliases: ["c1"], // Not implemented
+                    handler: __dirname + "/cli/get/resource/Test.login.handler",
+                    login: tempLogin,
+                    logout: tempLogout
+                },
+                {
+                    serviceName: "cics-002",
+                    // aliases: ["c2"], // Not implemented
+                    handler: __dirname + "/cli/get/resource/Test.logout.handler",
+                    login: tempLogin,
+                    logout: tempLogout
+                }
+            ],
             type: "cics",
             schema: {
                 type: "object",
@@ -37,6 +81,26 @@ const config: IImperativeConfig = {
                     "CICS regions. The cics profile contains your host, port, user name, and password " +
                     "for the IBM CICS management client interface (CMCI) server of your choice.",
                 properties: {
+                    tokenType: {
+                        type: "string",
+                        optionDefinition: {
+                            name: "cics-token-type",
+                            aliases: ["ctt"],
+                            description: "The type of the token to pass to the API.",
+                            type: "string",
+                            group: "CICS AUTH OPTIONS"
+                        }
+                    },
+                    tokenValue: {
+                        type: "string",
+                        optionDefinition: {
+                            name: "cics-token-value",
+                            aliases: ["ctv"],
+                            description: "The value of the token to pass to the API.",
+                            type: "string",
+                            group: "CICS AUTH OPTIONS"
+                        }
+                    },
                     host: {
                         type: "string",
                         optionDefinition: {
